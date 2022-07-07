c59727df4732efab652c83e676494fc73cc38ffd
Bagnarola Roy <broyettore@gmail.com>
2022-07-07 11:10:42 +0200

    sticky header

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+/*============ sticky navbar ===============*/
+
+
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+    header.classList.toggle ("sticky", window.scrollY > 0);
+});
